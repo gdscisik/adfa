9ebc1b47a1ac681e098f157299240ffa301c52bb
Osman Nuri ERDOGAN <osmerd04@gmail.com>
2023-03-18 14:31:58 +0300

    Update

--- v1/frontend/src/utils/appAxios.js
+++ v1/frontend/src/utils/appAxios.js
@@ -4,6 +4,7 @@ const PORT = 5173 || process.env.PORT;
 
 export const appAxios = axios.create({
   baseURL: `http://localhost:${PORT}`,
+
   // baseURL: `https://adfa-world-default-rtdb.firebaseio.com`,
   // baseURL: `https://localhost:5173`,
 });
