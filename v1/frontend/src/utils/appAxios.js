3978abba88af5fb3f2be71e07d00b2dff71483cd
Osman Nuri ERDOGAN <osmerd04@gmail.com>
2023-03-18 14:19:20 +0300

    Store initiated.

--- v1/frontend/src/utils/appAxios.js
+++ v1/frontend/src/utils/appAxios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-//const PORT = process.env.PORT || 5173;
+const PORT = 5173 || process.env.PORT;
 
 export const appAxios = axios.create({
-  // baseURL: `http://localhost:${PORT}`,
+  baseURL: `http://localhost:${PORT}`,
   // baseURL: `https://adfa-world-default-rtdb.firebaseio.com`,
-  baseURL: `https://localhost:5173`,
+  // baseURL: `https://localhost:5173`,
 });
 
 export default appAxios;
